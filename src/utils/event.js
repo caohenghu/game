0cdf05f3c6ea435eb1d257b516c95846a23301a6
caohenghu <caohenghu@gmail.com>
2020-02-08 16:09:36 +0800

    兼容ipad

--- src/utils/event.js
+++ src/utils/event.js
@@ -1,4 +1,5 @@
-import { isMobile } from './ua'
+// 不能通过userAgent来判断，因为ipad能打开桌面网站，就无法知道是ipad
+const isMobile = 'ontouchstart' in document
 
 const click = isMobile ? 'touchstart' : 'click'
 const mousedown = isMobile ? 'touchstart' : 'mousedown'
